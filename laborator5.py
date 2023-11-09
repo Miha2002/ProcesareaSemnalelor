99e081034346b2b88bb5e1be12a374b2765daec8
Miha <81815165+Miha2002@users.noreply.github.com>
2023-11-09 19:45:26 +0200

    Add files via upload

---
+++ laborator5.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ex1():
+    return 0
+
+
+if __name__ == "__main__":
+
+    # Exercitiul 1.
+    # 18288 esantioane
+    # intr-o secunda => ??
+
+    # Exercitiul 2.
+    # Considerand ca se precizeaza ca fiecare esantion reprezinta datele obtinute o data pe ora
+    # inseamna ca cele 18288 esantioane au fost obtiute in 762 de zile
+    # ??
+
+    #
+
+
+    ex1()
