f5086ae83fdf5e6e7f7341770f0daae0d9994d7c
Miha <81815165+Miha2002@users.noreply.github.com>
2023-11-16 13:12:31 +0200

    Update laborator5.py

--- laborator5.py
+++ laborator5.py
@@ -1,23 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+import csv
+import pandas as pd
+
+def pctD():
+    data = np.genfromtxt("Train.csv", delimiter=',', names=True, dtype=None, encoding=None)
+    semnal = data['Count']
+    N = len(semnal)
+    f = 1/3600
+
+    fft = np.fft.fft(semnal)
+    res = np.abs(fft)
+    n = np.fft.fftfreq(N, d=1/f)
+
+    plt.plot(n, res)
+    plt.xlabel("Frecventa(Hz)")
+    plt.ylabel("Fourier")
+    plt.show()
 
 
-def ex1():
-    return 0
+def pctE():
+    data = np.genfromtxt("Train.csv", delimiter=',', names=True, dtype=None, encoding=None)
+    time = pd.to_datetime(data["Datetime"], format='%d-%m-%Y %H:%M', errors='coerce')
+    semnal = data['Count']
+    N = len(semnal)
+    f = 1 / 3600
+
+    res = semnal.mean()
+    print(int(res))
+    if res != 0:
+        print("Semnalul are o componenta continua cu valoarea:", res)
+        time_diff = np.diff(time).mean()
+        time_diff_seconds = time_diff.astype('timedelta64[s]').view('int64') / 1e9
+        frequencies = np.fft.fftfreq(N, d=time_diff_seconds)
+        frequencies = frequencies[:N // 2]
+
+        semnal_nou = int((semnal - res).mean()) # foarte aproape de 0, erori ici colo idk
+        print("Semnalul nou are componenta continua:", semnal_nou, " ,adica nu are :)))")
+    else:
+        print("Semnalul nu are componenta continuă.")
 
 
 if __name__ == "__main__":
 
-    # Exercitiul 1.
-    # 18288 esantioane
-    # intr-o secunda => ??
+    # Punctul A.
+    # 18288 esantioane, 1 esantion / ora
+    # intr-o secunda => 1/(60*60) = 0.000277 Hz
 
-    # Exercitiul 2.
+    # Punctul B.
     # Considerand ca se precizeaza ca fiecare esantion reprezinta datele obtinute o data pe ora
     # inseamna ca cele 18288 esantioane au fost obtiute in 762 de zile
-    # ??
+    # 18288 / 24 = 762
 
-    #
+    # Punctul C.
+    # Conform Nyquist frecventa maxima este 1/2 din frecventa de esantionare
+    # => 0.000277 Hz / 2 = 0.000138 Hz
 
-
-    ex1()
+    # pctD()
+    pctE()
